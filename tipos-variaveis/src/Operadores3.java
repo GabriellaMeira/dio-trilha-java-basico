5ecc8f303c9f321baff4a7b4bd9f0ddfef2d43c9
GabriellaMeira <gabimeira2000@gmail.com>
2025-01-10 13:23:38 -0300

    aula de operadores 3

---
+++ tipos-variaveis/src/Operadores3.java
@@ -0,0 +1,15 @@
+public class Operadores3 {
+    
+    public static void main (String[] args){
+        int numero = 5;
+        //x repeticao 
+       
+        //numero = numero + 2;
+
+        System.out.println(numero--);
+
+        System.out.println(numero);
+
+
+    }
+}
