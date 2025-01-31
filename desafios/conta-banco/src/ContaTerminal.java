fc9fa0666364309991a8c1df9aea79e82827f4a1
GabriellaMeira <gabimeira2000@gmail.com>
2025-01-31 12:10:36 -0300

    ajustes realizados

--- desafios/conta-banco/src/ContaTerminal.java
+++ desafios/conta-banco/src/ContaTerminal.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+        
+        //TODO: Conhecer e importar a classe scanner
         Scanner scanner = new Scanner(System.in);
-
-       //TODO: Conhecer e importar a classe scanner
         
        //Exibir as mensagens para o nosso usuário 
        System.out.print("Por favor, digite o número da Conta: ");
        int numero = scanner.nextInt();
 
-       scanner.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+       scanner.nextLine(); 
 
        System.out.print("Por favor, digite o número da Agência: ");
        String agencia = scanner.nextLine();
@@ -21,14 +21,13 @@ public class ContaTerminal {
        System.out.print("Por favor, digite o saldo inicial: ");
        double saldo = scanner.nextDouble();
 
-       System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
-       System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque.");
-
-       System.out.println("\nConta criada com sucesso!\n");
-
-
-        scanner.close();
+       System.out.println("\nOlá " + nomeCliente);
+       System.out.println("Sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo);
 
        //Exibir a mensagem conta criada
+       System.out.println("\nConta criada com sucesso!\n");
+        
+        scanner.close();
+       
     }
 }
